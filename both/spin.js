586a3985ee916a664e3e611233853c1d5565df6d
AnnaRapp <amrapp4@gmail.com>
2016-04-24 20:34:16 -0400

    updated the home screen to be about our game

--- both/spin.js
+++ both/spin.js
@@ -13,5 +13,8 @@ Spin = {
 		var random = Math.floor(Math.random() * 5);
 		return values[random];
   };
-  scope.spinVal = Spin.getSpin();
+  scope.spinVal = Spin.getSpin(); //this is the key
+  //otherwise, it keeps generating random numbers
+  //each time you call it, instead of saving the
+  //one random number for each spin
 })(Spin);
--- client/views/spinnerArea/spinnerArea.js
+++ client/views/spinnerArea/spinnerArea.js
@@ -18,19 +18,5 @@ Template.spinnerArea.events({
 		event.preventDefault();
 		//alert("Spinning!");
 		location.reload(true);
-		// spinValue = Spin.getSpin();
-		// console.log("newSpinValue is "+spinValue);
-		// var spin = 0;
-		// Meteor.call('getSpin', function(error, result) {
-    //   		if (error)
-    //     		return alert(error.reason);
-    //     	else {
-    //     		alert(result);
-    //     		spin = result;
-    //     		console.log("new spin value = " +Session.get('spinValue'));
-    // 		}
-    // 	});
-    // 	Session.set('spinValue', result);
-
 	},
 });
